80413e8cd13c740406874ed8d5ae3b2f76d1184c
ramanakamadi <ramana.k@exilant.com>
2019-02-07 18:00:42 +0530

    first commite ramana

---
+++ src/main/java/com/exilant/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.exilant.demo.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Demo")
+public class TestController {
+
+	@GetMapping("/hello")
+	public String  getDemo(@RequestBody String str,HttpRequest req){
+		
+		
+			
+		return "Hello";
+	}
+	
+}
