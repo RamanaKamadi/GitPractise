7751d43ff951c2e44aaf5b976df28c29bf443058
ramanakamadi <ramana.k@exilant.com>
2019-02-11 16:48:55 +0530

    first and second commit

--- src/main/java/com/exilant/demo/controller/TestController.java
+++ src/main/java/com/exilant/demo/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.exilant.demo.controller;
+  package com.exilant.demo.controller;
 
 import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +14,9 @@ public class TestController {
 	@GetMapping("/hello")
 	public String  getDemo(@RequestBody String str,HttpRequest req){
 
-		System.out.println("hi.....Quest");
-	    System.out.println("devi...");
-        System.out.println("hi.....Quest");
-        System.out.println("helooooo");
-		System.out.println("Welcome :::::devi");
-		System.out.println("Rama");
-		return "Hello";
+		System.out.println("first....");
+	        System.out.println("second......");
+    		return "Hello";
 	}
 	
 }
